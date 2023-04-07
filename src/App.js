44120fa7cc7075d5b31962ac5b5fc9e0d7c22f04
CosmicButters <93351528+CosmicButters@users.noreply.github.com>
2023-04-07 17:44:47 -0400

    Adding Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Nodes from "./pages/Nodes";
 
 function App() {
     return (
-        <div className="App">
-            <h1>Atlas</h1>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/nodes" element={<Nodes />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </Router>
     );
 }
 export default App;
---
+++ src/pages/Nodes.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Nodes() {
+    return (
+        <div>
+            <h1>Nodes</h1>
+        </div>
+    );
+}
+
+export default Nodes;
